c39d1e8f97e7b8cc6ee9eb7043ddae5511fc09e2
William Falcone <wfalcone@mparticle.com>
2019-08-13 17:26:40 -0400

    identity and events

--- Implement/mp_implement.js
+++ Implement/mp_implement.js
@@ -1,37 +1,82 @@
-function login(){
+//Remove "Not Setup" once function is working
 
+function login(){
+    //create an identity request obj 
+    //create an identitiy callback - hint - in the correct location
+    //call the MP Login Function
+    console.log("not setup")
 }
 
 
 
 function modify(){
+    //create identity request obj - with new value for email
+    // call MP Modify Function
+    console.log("not setup")
 
 }
 
 
 function logout(){
+    //Call logout function
+    console.log("not setup")
 
 }
 
 function setUserAttribute(){
+    //get current user
+    //set a user attribute
+    console.log("not setup")
 
 }
 
 
-function logScreen(){
+function logPageView(){
+    //log MP Page View
+    console.log("not setup")
 
 }
 
-function testEvent(){
+function logEvent(){
+    //log MP logEvent
+    console.log("not setup")
 
 }
 
 
 function seach(){
+    console.log("not setup")
 
 }
 
 
 function impression(){
-    
+    console.log("not setup")
+
 }
+
+function viewDetail(){
+    console.log("not setup")
+
+}
+
+function addToCart(){
+    console.log("not setup")
+
+}
+
+function removeFromCart(){
+    console.log("not setup")
+
+}
+
+function checkout(){
+    console.log("not setup")
+
+}
+
+function purchase(){
+    console.log("not setup")
+
+}
+
