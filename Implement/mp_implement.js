7319abf499ec91dd2c025dd7e75195d3f66f5cb8
William Falcone <wfalcone@mparticle.com>
2019-08-13 16:51:34 -0400

    first push

---
+++ Implement/mp_implement.js
@@ -0,0 +1,37 @@
+function login(){
+
+}
+
+
+
+function modify(){
+
+}
+
+
+function logout(){
+
+}
+
+function setUserAttribute(){
+
+}
+
+
+function logScreen(){
+
+}
+
+function testEvent(){
+
+}
+
+
+function seach(){
+
+}
+
+
+function impression(){
+    
+}
